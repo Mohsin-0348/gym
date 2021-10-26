1c9828fc0ceace557b5b3bf9a1f160df4821e270
jim <mohsin.mahbub.jim@gmail.com>
2021-10-26 10:48:01 +0600

    changes for allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', None)]
 
 
 # Application definition

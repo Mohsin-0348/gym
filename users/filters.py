55b0901559aab818e6bbdd9f636a2dd06aab45eb
jim <mohsin.mahbub.jim@gmail.com>
2021-10-27 11:59:40 +0600

    changes for filters

--- users/filters.py
+++ users/filters.py
@@ -20,15 +20,6 @@ class UserFilters(BaseFilters):
         lookup_expr='icontains'
     )
 
-    first_name = django_filters.CharFilter(
-        field_name='first_name',
-        lookup_expr='icontains'
-    )
-    last_name = django_filters.CharFilter(
-        field_name='last_name',
-        lookup_expr='icontains'
-    )
-
     class Meta:
         model = User
         fields = [
